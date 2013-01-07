9e7647a0be0a402ce8c84283df58fec300462010
archer16 <archer.megadrive@gmail.com>
2013-01-07 10:20:03 +0100

    Added php script to convert RGB pallete into Genesis safe colors. (not yet complete)

---
+++ data/tools/genesis_palette.php
@@ -0,0 +1,54 @@
+<?php
+  $input = file_get_contents('php://stdin');
+  $lines = explode($input,"\n");
+  print_r( $lines);
+
+  genesis_level[0] = 0;
+  genesis_level[1] = 36;
+  genesis_level[2] = 73;
+  genesis_level[3] = 108
+  genesis_level[4] = 144;
+  genesis_level[5] = 180;
+  genesis_level[6] = 216;
+  genesis_level[7] = 252;
+
+
+
+
+
+
+  
+/*  
+  vdp_palette['normal'][0] = 0;
+  vdp_palette['normal'][1] = 36;
+  vdp_palette['normal'][2] = 73;
+  vdp_palette['normal'][3] = 108
+  vdp_palette['normal'][4] = 144;
+  vdp_palette['normal'][5] = 180;
+  vdp_palette['normal'][6] = 216;
+  vdp_palette['normal'][7] = 252;
+  
+  vdp_palette['shadow'][0] = 0;
+  vdp_palette['shadow'][1] = 18;
+  vdp_palette['shadow'][2] = 36;
+  vdp_palette['shadow'][3] = 54
+  vdp_palette['shadow'][4] = 72;
+  vdp_palette['shadow'][5] = 90;
+  vdp_palette['shadow'][6] = 108;
+  vdp_palette['shadow'][7] = 126;
+  
+  vdp_palette['highlight'][0] = 126;
+  vdp_palette['highlight'][1] = 144;
+  vdp_palette['highlight'][2] = 162;
+  vdp_palette['highlight'][3] = 180
+  vdp_palette['highlight'][4] = 198;
+  vdp_palette['highlight'][5] = 216;
+  vdp_palette['highlight'][6] = 234;
+  vdp_palette['highlight'][7] = 252;
+  
+  //Original values: Normal :              0,    36,    72,   108,   144,   180,   216, 252
+  //Original values: Shadow :              0,    18,    36,    54,    72,    90,   108,   126
+  //Original values: Highlight :         126,   144,   162,   180,   198,   216,   234,   252
+*/
+  
+?>
