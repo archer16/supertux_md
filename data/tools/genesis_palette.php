208de069890a726e0dd1ea0aa936eb45637b0a01
archer16 <archer.megadrive@gmail.com>
2013-01-19 15:55:17 +0100

    Made script genesis_palette.php convert 24 bit RGB into VDP compatible levels

--- data/tools/genesis_palette.php
+++ data/tools/genesis_palette.php
@@ -1,54 +1,51 @@
 <?php
-  $input = file_get_contents('php://stdin');
-  $lines = explode($input,"\n");
-  print_r( $lines);
+  $lines = explode("\n",file_get_contents('php://stdin'));
+  //print_r( $lines);
+  echo $lines[0]."\n";
 
-  genesis_level[0] = 0;
-  genesis_level[1] = 36;
-  genesis_level[2] = 73;
-  genesis_level[3] = 108
-  genesis_level[4] = 144;
-  genesis_level[5] = 180;
-  genesis_level[6] = 216;
-  genesis_level[7] = 252;
-
-
-
-
-
-
+  $vdp_level[0] = 0;
+  $vdp_level[1] = 36;
+  $vdp_level[2] = 73;
+  $vdp_level[3] = 108;
+  $vdp_level[4] = 144;
+  $vdp_level[5] = 180;
+  $vdp_level[6] = 216;
+  $vdp_level[7] = 252;
+  $vdp_level[8] = 255;
   
-/*  
-  vdp_palette['normal'][0] = 0;
-  vdp_palette['normal'][1] = 36;
-  vdp_palette['normal'][2] = 73;
-  vdp_palette['normal'][3] = 108
-  vdp_palette['normal'][4] = 144;
-  vdp_palette['normal'][5] = 180;
-  vdp_palette['normal'][6] = 216;
-  vdp_palette['normal'][7] = 252;
+  build_vdp_list();
   
-  vdp_palette['shadow'][0] = 0;
-  vdp_palette['shadow'][1] = 18;
-  vdp_palette['shadow'][2] = 36;
-  vdp_palette['shadow'][3] = 54
-  vdp_palette['shadow'][4] = 72;
-  vdp_palette['shadow'][5] = 90;
-  vdp_palette['shadow'][6] = 108;
-  vdp_palette['shadow'][7] = 126;
-  
-  vdp_palette['highlight'][0] = 126;
-  vdp_palette['highlight'][1] = 144;
-  vdp_palette['highlight'][2] = 162;
-  vdp_palette['highlight'][3] = 180
-  vdp_palette['highlight'][4] = 198;
-  vdp_palette['highlight'][5] = 216;
-  vdp_palette['highlight'][6] = 234;
-  vdp_palette['highlight'][7] = 252;
-  
-  //Original values: Normal :              0,    36,    72,   108,   144,   180,   216, 252
-  //Original values: Shadow :              0,    18,    36,    54,    72,    90,   108,   126
-  //Original values: Highlight :         126,   144,   162,   180,   198,   216,   234,   252
-*/
-  
+  for($x=1;$x<count($lines);$x++)
+  {
+    if(!$lines[$x]=='')
+    {
+      $base    = strpos($lines[$x],'(');
+      $comment = strpos($lines[$x],'#') -2;
+      $pixel   = substr($lines[$x],0,$comment);
+      
+      $rgba = explode(',',substr($pixel,$base+1,$comment - $base - 2));
+      $rgba[0] = find_vdp_color($rgba[0]);
+      $rgba[1] = find_vdp_color($rgba[1]);
+      $rgba[2] = find_vdp_color($rgba[2]);
+      echo substr_replace($pixel,join(',',$rgba),$base+1,15)."\n";
+    }
+  }
+
+  function find_vdp_color($value)
+  {
+    global $vdp_levels;
+    
+    return substr('   '.$vdp_levels[trim($value)],-3,3);
+  }
+
+  function build_vdp_list()
+  {
+    global $vdp_level;
+    global $vdp_levels;
+    
+    for($x=0;$x<256;$x++)
+    {
+      $vdp_levels[$x] = $vdp_level[round((8.0 / 255) * $x,PHP_ROUND_HALF_UP)];
+    }
+  }
 ?>
